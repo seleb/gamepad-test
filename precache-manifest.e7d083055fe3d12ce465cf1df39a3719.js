ff38f2afc324c14f3c511518a231af84790a4d6e
seleb <seleb@users.noreply.github.com>
2022-02-10 23:25:36 +0000

    deploy: 3883dd4518a8350a5dce62752d098ba1a90e8d80

---
+++ precache-manifest.e7d083055fe3d12ce465cf1df39a3719.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f212193eba0766953ab63e576af9e4",
+    "url": "/gamepad-test/index.html"
+  },
+  {
+    "revision": "4e2f877e5f44118e0b1e",
+    "url": "/gamepad-test/static/css/main.aba04ead.chunk.css"
+  },
+  {
+    "revision": "d6415a396ad8f080b6f1",
+    "url": "/gamepad-test/static/js/2.023ab2dd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/gamepad-test/static/js/2.023ab2dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2f877e5f44118e0b1e",
+    "url": "/gamepad-test/static/js/main.9e5840c4.chunk.js"
+  },
+  {
+    "revision": "d550e50d0255dfb3f5c4",
+    "url": "/gamepad-test/static/js/runtime-main.99c18ab6.js"
+  }
+]);
